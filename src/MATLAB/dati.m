0a97ee24e3df35b50e9b182ce837d232b7257d99
Lorenzo Bianchi <lnz.bnc@gmail.com>
2024-12-02 18:48:14 +0100

    chore: update dati.m

--- src/MATLAB/dati.m
+++ src/MATLAB/dati.m
@@ -6,39 +6,39 @@ DISEGNA_ULTIMO = 0;
 DISEGNA_PLOT = 0;
 DISEGNA_ICP = 0;
 DISEGNA_VAR = 0;
-DISEGNA_POSA = 1;
+DISEGNA_POSA = 0;
 displayErrori = 0;
 
 data = struct();
 
-nRobot = 6;
+nRobot = 10;
 
 nPassi = 1500;
 nPhi = 8; % numero ipotesi angolo (si può poi variare in funzione della distanza misurata)
 pruning = 1;
-minZerosStartPruning = ceil(nPhi*0.6);
-stepStartPruning0 = 100;         % mettere valore piccolo per evitare errori iniziali
+minZerosStartPruning = ceil(nPhi*0.3);
+stepStartPruning0 = 10;         % mettere valore piccolo per evitare errori iniziali
 stepStartPruning = repmat({stepStartPruning0}, 1, nRobot);
 sharing = 1;
-stepStartSharing = 400;
+stepStartSharing = 600;
 reset = 1;
-resetThr = 100;
+resetThr = 20;
 
-sigmaDistanza = 0.2; % std in m della misura di range
+sigmaDistanza = 0.3; % std in m della misura di range
 sigmaDistanzaModello = sigmaDistanza; % Per prevedere anche un'incertezza sulla deviazione standard dell'errore di misura
-sigmaMisuraMedia = 1.0;
+sigmaMisuraMedia = 0.5;
 
 % ransac
 numIterationsA = 120;       % other tags -> other tags
 distanceThresholdA = 0.2;
 percentMinInliersA = 0.4;
-numIterationsB = 120;       % this tag ->other tags
+numIterationsB = 120;       % this tag -> other tags
 distanceThresholdB = 0.1;
 percentMinInliersB = 0.6;
 
 
 
-Nstep = 1; % passi tra una misura e la successiva
+Nstep = 10; % passi tra una misura e la successiva
 possibiliPhi = linspace(-pi+2*pi/nPhi, pi, nPhi);
 sigmaPhi = 2*pi/(1.5*nPhi); %pi/(3*nPhi);
 
@@ -47,7 +47,7 @@ L = 10; % lunghezza lato ambiente (quadrato)
 % Caratteristiche robot: si assume di aver calibrato l'odometria per
 % l'errore sistematico ma ciò non impedisce di considerare che qualche
 % erroretto c'è rimasto...
-dVera = 0.26; % distanza presunta tra le due ruote
+dVera = 0.16; % distanza presunta tra le due ruote
 deltaRvera = 1; % fattore collegato con diametro ruota destra
 deltaLvera = 1; % fattore collegato con diametro ruota sinistra
 % ERRORE SISTEMATICO
